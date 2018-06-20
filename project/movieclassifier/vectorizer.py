4bdfc191494a2da3ada0f18c0901ba13a65ff2bc
tango911 <29842684+tango911@users.noreply.github.com>
2018-06-20 13:00:29 +0530

    Sentiment Analysis
    I take a supervised approach to the problem and 50,000 movie reviews for building our training data. I investigate an efficient method to build a strong model for extracting the features that contain sentimental information and deploy it on a web page.

---
+++ project/movieclassifier/vectorizer.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 23 05:51:30 2018
+
+@author: MOHIT
+"""
+
+from sklearn.feature_extraction.text import HashingVectorizer
+import re
+import os
+import pickle
+
+
+cur_dir = os.path.dirname('G:\code\python\sentiment_analisis\movieclassifier\pkl_objects')
+stop = pickle.load(open(
+                   os.path.join(cur_dir,
+                  'pkl_objects',
+                  'stopwords.pkl'), 'rb'))
+def tokenizer(text):
+     text = re.sub('<[^>]*>', '', text)
+     emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)',
+                                    text.lower())
+     text = re.sub('[\W]+', ' ', text.lower()) \
+ + ' '.join(emoticons).replace('-', '')
+     tokenized = [w for w in text.split() if w not in stop]
+     return tokenized
+vect = HashingVectorizer(decode_error='ignore',
+                         n_features=2**21,
+                         preprocessor=None,
+                         tokenizer=tokenizer)
